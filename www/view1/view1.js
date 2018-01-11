4ec84eb85aff55bcfecb9f98d26b87a14308d528
Smari Alfredsson <smari@apollomusic.dk>
2018-01-11 20:32:52 +0100

    First commit

---
+++ www/view1/view1.js
@@ -0,0 +1,42 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute', 'angular-web-notification'])
+
+.config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/view1', {
+		templateUrl: 'view1/view1.html',
+		controller: 'View1Ctrl'
+	});
+}])
+
+.controller('View1Ctrl', ['$scope','$http', function ($scope, $http) {
+
+ $scope.getDetails = function (index, matches) {
+    console.log("You clicked: " + matches.event); // this works fine.
+    $scope.myChoice = event; // ReferenceError: myChoice is not defined
+
+    console.log($scope.myChoice);
+  }
+
+
+	$http.get("http://hltv-api.herokuapp.com/v1/matches?type=upcoming")
+	.success(function(response) {
+
+		$scope.details = response;
+
+		//Inititate empty array
+		$scope.allMatches = [];
+
+		//Loop through the JSON data response
+		angular.forEach($scope.details, function(item){
+			$scope.allMatches.push(item);
+			console.log($scope.allMatches)
+		})
+
+	});
+
+
+
+}]);
+
+
---
+++ www/view2/view2.js
@@ -0,0 +1,49 @@
+'use strict';
+
+angular.module('myApp.view2', ['ngRoute', 'angular-web-notification'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view2', {
+    templateUrl: 'view2/view2.html',
+    controller: 'View2Ctrl'
+  });
+}])
+
+.controller('View2Ctrl', ['$scope', function ($scope) {
+    'use strict';
+
+    $scope.title = 'G2 vs FaZe | 18:00 | bo3';
+    $scope.text = 'Match is about to start! Link: hltv.org';
+}]).directive('showButton', ['webNotification', function (webNotification) {
+    'use strict';
+
+    return {
+        restrict: 'C',
+        scope: {
+            notificationTitle: '=',
+            notificationText: '='
+        },
+        link: function (scope, element) {
+            element.on('click', function onClick() {
+                webNotification.showNotification(scope.notificationTitle, {
+                    body: scope.notificationText,
+                    onClick: function onNotificationClicked() {
+                        console.log('Notification clicked.');
+                    },
+                    autoClose: 4000 //auto close the notification after 4 seconds (you can manually close it via hide function)
+                }, function onShow(error, hide) {
+                    if (error) {
+                        window.alert('Unable to show notification: ' + error.message);
+                    } else {
+                        console.log('Notification Shown.');
+
+                        setTimeout(function hideNotification() {
+                            console.log('Hiding notification....');
+                            hide(); //manually close the notification (you can skip this if you use the autoClose option)
+                        }, 5000);
+                    }
+                });
+            });
+        }
+    };
+}]);
